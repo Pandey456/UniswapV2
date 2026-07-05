3cb9ce0c0d72db34074fe12d7dcc6815bf6e7b09
Adarsh Pandey <pandey.adarsh2910@gmail.com>
2026-07-05 12:53:12 +0530

    sepolia

--- script/deployV2.s.sol
+++ script/deployV2.s.sol
@@ -8,14 +8,15 @@ import {Router} from "../src/Router.sol";
 contract deployV2 is Script {
     factory public Factory;
     Router public router;
+    address existingFactory = 0xe072286F025dABa307478c279d299EE3046A289e;
 
     function run() public {
         vm.startBroadcast();
-        Factory = new factory();
-        router = new Router(address(Factory));
+        // Factory = new factory();
+        router = new Router(existingFactory);
         vm.stopBroadcast();
         console2.log("=== Sepolia Deployment Successful ===");
-        console2.log("Factory Address:", address(Factory));
+        console2.log("Factory Address:", existingFactory);
         console2.log("Router Address: ", address(router));
         console2.log("=====================================");
     }
